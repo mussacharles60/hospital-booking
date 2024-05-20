2ae47288a77b4e7fe79dd94254f674f40f945694
Mussa Charles <mussacharles60@gmail.com>
2024-05-20 19:19:54 +0300

    Update index.d.ts

--- frontend/web/src/models/index.d.ts
+++ frontend/web/src/models/index.d.ts
@@ -4,25 +4,24 @@ import { Department } from './department';
 import { DepartmentDoctor, Doctor } from './doctor';
 import { Patient } from './patient';
 
-export type APIResponseType = {
-  success?: APISuccessResponseType;
-  error?: APIErrorResponseType;
+export type APIResponse<T = undefined> = {
+  success?: APISuccessResponse<T>;
+  error?: APIErrorResponse;
 };
 
-export type APISuccessResponseType = {
+export type APISuccessResponse<T> = {
   code?: number;
   message?: string;
-  data?: APIResponseData;
+  data?: T;
 };
 
-export type APIErrorResponseType = {
+export type APIErrorResponse = {
   code?: number;
   message?: string;
+  reason?: 'invalid_token' | 'token_expired';
 };
 
 export type APIResponseData = {
-  token?: string;
-  token_type?: 'Bearer';
   admin?: Admin;
   doctor?: Doctor;
   department?: Department;
@@ -33,4 +32,23 @@ export type APIResponseData = {
   appointments?: Appointment[];
   department_doctor?: DepartmentDoctor;
   department_doctors?: DepartmentDoctor[];
+};
+
+export type AuthResponseData = {
+  access_token?: string;
+  token_type?: 'Bearer';
+  admin?: Admin;
+  doctor?: Doctor;
+  patient?: Patient;
+  password_recover_email_sent?: boolean;
+};
+
+export type AuthUser = {
+  access_token?: string | null;
+  user: Admin | Doctor | Patient | null;
+};
+
+export type AuthContextType = {
+  auth: AuthUser | null;
+  setAuth: (auth: AuthUser | null) => void;
 };
