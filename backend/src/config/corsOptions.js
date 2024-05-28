d36cd10fce3ad58b7267feab7d0f1779f7b7a86a
Mussa Charles <mussacharles60@gmail.com>
2024-05-28 18:14:56 +0300

    Update corsOptions.js

--- backend/src/config/corsOptions.js
+++ backend/src/config/corsOptions.js
@@ -1,5 +1,23 @@
+const allowedOrigins = require('./allowedOrigins');
+const ResponseCodes = require('../apis');
+
 const corsOptions = {
-  origin: 'http://localhost',
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(
+        new Error(
+          JSON.stringify({
+            err: {
+              code: ResponseCodes.ClientError.UNAUTHORIZED,
+              msg: 'unauthorized',
+            },
+          })
+        )
+      );
+    }
+  },
   optionsSuccessStatus: 200,
   exposedHeaders: ['x-auth-token'],
 };
